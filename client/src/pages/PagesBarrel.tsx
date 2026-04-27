e1c60081db71909c9dade21426df9dd7d5cecef5
Mónica Guzmán Molina <m0nigumol@gmail.com>
2026-04-27 01:17:33 -0600

    feat: add PagesBarrel file to streamline page imports

---
+++ client/src/pages/PagesBarrel.tsx
@@ -0,0 +1,13 @@
+/* 
+THIS IS A BARREL FILE, IT HELPS TO IMPORT ALL THE PAGES
+IN THE PAGES FOLDER WITH A SINGLE IMPORT
+ALLOWING TO HAVE A CLEANER IMPORTS IN THE APP
+*/
+
+import LogIn from './LogIn';
+import SignUp from './SignUp';
+import Home from './Home';
+import ForgotPassword from './ForgotPassword';
+
+export { LogIn, SignUp, Home, ForgotPassword };
+
