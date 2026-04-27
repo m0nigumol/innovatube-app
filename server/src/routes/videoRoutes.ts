799b0387b327290b061b41c4b221dd31ab667a13
Mónica Guzmán Molina <m0nigumol@gmail.com>
2026-04-27 06:49:04 -0600

    Feat: Update video routes to remove favorites management and add load/search functionalities

--- server/src/routes/videoRoutes.ts
+++ server/src/routes/videoRoutes.ts
@@ -1,17 +1,14 @@
 import { Router } from "express";
-import { addFavorite, removeFavorite, getFavorites } from "../controllers/videoController";
+import { loadVideos, searchVideos } from "../controllers/videoController";
 import verifyToken from "../middlewares/authMiddleware";
 import { get } from "mongoose";
 
 const router = Router();
 
 // THESE ROUTES REQUIRE TO BE LOGGED IN
-
-// ROUTE: POST /api/video/favorites
-router.post('/favorites', verifyToken, addFavorite);
-// ROUTE: DELETE /api/video/favorites   
-router.delete('/favorites', verifyToken, removeFavorite);
-// ROUTE: GET /api/video/favorites
-router.get('/favorites', verifyToken, getFavorites);
+// ROUTE: GET /api/videos
+router.get('/load', verifyToken, loadVideos);
+// ROUTE: GET /api/videos
+router.get('/search', verifyToken, searchVideos);
 
 export default router;
