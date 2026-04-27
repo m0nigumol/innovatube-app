bdf6e1bc9b4ae21bc09b12870d1fc62d6c6fa02f
Mónica Guzmán Molina <m0nigumol@gmail.com>
2026-04-27 06:54:43 -0600

    Refactor: Remove favorite management functions and streamline video loading and searching functionalities

--- server/src/controllers/videoController.ts
+++ server/src/controllers/videoController.ts
@@ -1,77 +1,37 @@
 import { Request, Response } from "express";
+import axios from 'axios';
 import User from "../models/User";
 
-export const addFavorite = async (req: any, res: Response) => {
+
+export const loadVideos = async (req: Request, res: Response) => {
+    const API_KEY = process.env.YOUTUBE_API_KEY;
+    const url = `https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=12&regionCode=MX&key=${API_KEY}`;
+
     try {
-        const user = await User.findById(req.user.id);
-        const videoId = req.body;
-
-        if (!user) {
-            return res.status(404).json({ message: "[ ❌ USER ERROR ]: User not found" });
-        }
-
-        if (!videoId) {
-            return res.status(400).json({ message: "[ ❌ ADD VIDEO ERROR ]: Video ID is required" });
-        }
-
-        // CHECK IF VIDEO ID IS ALREADY IN FAVORITES
-        if (user.favorites.includes(videoId)) {
-            return res.status(400).json({ message: "[ ❌ ADD VIDEO ERROR ]: Video is already in favorites" });
-        }
-
-        // ADD VIDEO ID TO FAVORITES
-        user.favorites.push(videoId);
-        // SAVE THE UPDATED USER DOCUMENT
-        await user.save();
-
-        // RESPOND WITH SUCCESS MESSAGE
-        res.status(200).json({ message: "[ ✅ SUCCESS ]: Video added to favorites" });
+        // GET VIDEOS
+        console.log(['[ 📹 FETCHING VIDEOS ]: Fetching from --> ', url]);
+        const response = await axios.get(url);
+        // SEND RESPONSE
+        res.json(response.data);
+        console.log('[ 🎉 SUCCESS ]: Videos fetched successfully');
     } catch (error) {
-        res.status(500).json({ message: `[ ❌ ADD FAVORITE ERROR ]: ${error}` });
+        res.status(500).json({ error: "Error al cargar recomendaciones" });
     }
 };
-
-export const removeFavorite = async (req: any, res: Response) => {
-    try {
-        const user = await User.findById(req.user.id);
-        const videoId = req.body;
-
-        if (!user) {
-            return res.status(404).json({ message: "[ ❌ USER ERROR ]: Null user" });
-        }
-
-        if (!videoId) {
-            return res.status(400).json({ message: "[ ❌ REMOVE VIDEO ERROR ]: Null video id" });
-        }
-
-        // CHECK IF VIDEO ID IS IN FAVORITES
-        if (!user.favorites.includes(videoId)) {
-            return res.status(400).json({ message: "[ ❌ REMOVE VIDEO ERROR ]: Video is not in favorites" });
-        }
-
-        // FILTER ALL VIDEO IDS EXCEPT THE ONE TO BE REMOVED
-        user.favorites = user.favorites.filter((id: string) => id !== videoId);
-        // SAVE THE UPDATED USER DOCUMENT
-        await user.save();
-
-        // RESPOND WITH SUCCESS MESSAGE
-        res.status(200).json({ message: "[ ✅ SUCCESS ]: Video removed from favorites" });
-    } catch (error) {
-        res.status(500).json({ message: `[ ❌ REMOVE VIDEO ERROR ]: ${error}` });
-    }
-};
-
-export const getFavorites = async (req: any, res: Response) => {
+export const searchVideos = async (req: Request, res: Response) => {
+    const { q } = req.query;
+    const API_KEY = process.env.YOUTUBE_API_KEY;
+    const url =
+        `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=12&q=${q}&type=video&key=${API_KEY}`
 
     try {
-        const user = await User.findById(req.user.id);
-        if (!user) {
-            return res.status(404).json({ message: "[ ❌ GET FAVORITES ERROR ]: Null user" });
-        }
-
-        // RETURN THE FAVORITES ARRAY OR AN EMPTY ARRAY IF USER NOT FOUND
-        res.status(200).json(user?.favorites || []);
-    } catch (error) {
-        res.status(500).json({ message: `[ ❌ GET FAVORITES ERROR ]: ${error}` });
+        // SEARCH VIDEO QUERY
+        console.log([`[ 📨 SEARCHING VIDEO ]: Searching ${q} in --> `, url]);
+        const response = await axios.get(url);
+        // SEND RESPONSE
+        res.json(response.data);
+    } catch (error: any) {
+        console.error("ERROR DE YOUTUBE:", error.response?.data || error.message);
+        res.status(500).json({ error: "Error al conectar con YouTube" });
     }
 };
